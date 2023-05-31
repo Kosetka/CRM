8fd6d2b91090fe377df80c28a696f3a37aa99b04
Kosetka <mateusz.zybura@gmail.com>
2023-05-31 13:45:08 +0200

    Final test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
-import ListGroup from "./components/ListGroup";
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
-  return <div><ListGroup /></div>
+  const [alertVisible, setAlertVisibility] = useState(false);
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>Treść alertu</Alert>
+      )}
+
+      <Button onClick={() => setAlertVisibility(true)}>Apply</Button>
+    </div>
+  );
 }
 
 export default App;
+
+/*
+
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = ["Radom", "Warszawa", "Kielce", "Gdańsk", "Szczecin"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
+}
+
+export default App;
+
+
+*/
